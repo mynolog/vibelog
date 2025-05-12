fed95aa6904e6eb4f10f921259cd1959738258f9
mynolog <minholee.log@gmail.com>
2025-05-12 22:23:46 +0900

    chore: initialize project with Angluar and TailwindCSS setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+import { EmotionFormComponent } from './components/emotion-form/emotion-form.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule, EmotionFormComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'vibelog';
---
+++ src/app/components/emotion-form/emotion-form.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-emotion-form',
+  imports: [],
+  templateUrl: './emotion-form.component.html',
+})
+export class EmotionFormComponent {}
